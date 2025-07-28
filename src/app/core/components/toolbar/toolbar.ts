8211f0e234a3728c6c3edffd4f1742ad4ead4262
Arturo Silvelo <asilvelo@trynewroads.com>
2025-07-28 13:09:41 +0200

    feat: implement user profile display and logout functionality in toolbar component

--- src/app/core/components/toolbar/toolbar.ts
+++ src/app/core/components/toolbar/toolbar.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { AuthService } from '@tnr/features/auth/services/auth.service';
+import { derivedAsync } from 'ngxtension/derived-async';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [MatIcon, MatButtonModule],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss',
 })
-export class Toolbar {}
+export class Toolbar {
+  readonly #authService = inject(AuthService);
+
+  me = derivedAsync(() => this.#authService.me());
+
+  onLogout() {
+    this.#authService.logout();
+  }
+}
--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { LoginDto } from '@tnr/features/auth/dto/login.dto';
+import { UserDto } from '@tnr/features/auth/dto/user.dto';
+import { ConfirmDialog } from '@tnr/shared/components/confirm-dialog/confirm-dialog';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   readonly #http = inject(HttpClient);
+  readonly #dialog = inject(MatDialog);
   readonly #router = inject(Router);
   readonly #slug = 'api/auth';
 
@@ -18,10 +22,26 @@ export class AuthService {
   }
 
   me() {
-    return this.#http.get(`${this.#slug}/me`);
+    return this.#http.get<UserDto>(`${this.#slug}/me`);
   }
 
   logout() {
-    return this.#http.delete(`${this.#slug}/me`, {});
+    this.#dialog
+      .open(ConfirmDialog, {
+        data: {
+          title: 'Cerrar Sesión',
+          message: '¿Estás seguro de que quieres cerrar sesión?',
+          cancelButtonText: 'Cancelar',
+          confirmButtonText: 'Cerrar Sesión',
+        },
+      })
+      .afterClosed()
+      .subscribe((confirm) => {
+        if (confirm) {
+          this.#http
+            .delete(`${this.#slug}/me`, {})
+            .subscribe(() => this.#router.navigate(['/']));
+        }
+      });
   }
 }
